5e779ac2ae43a28dd25a8211a6d27a6810159843
shnolk <0fast0furious@gmail.com>
2021-08-09 14:19:23 -0500

    Implemented image lazyload, restructured nav

--- assets/theme.js
+++ assets/theme.js
@@ -9,3 +9,27 @@ window.onscroll = function() {
     }
     prevPos = currentPos
 }
+
+
+const images = document.querySelectorAll('[data-original]');
+
+for (let image of images) {
+	const filename = image.dataset.original
+	const fullImg = new Image()
+	fullImg.src = filename
+	fullImg.onload = () => {
+		setTimeout(() => {
+			image.src = filename
+		}, 1000)
+	}
+}
+
+//const pixelator = document.querySelector('img')
+//const filename = pixelator.dataset.original
+
+//const image = new Image()
+//image.src = filename
+
+//image.onload = () => {
+//	pixelator.src=filename
+//}
